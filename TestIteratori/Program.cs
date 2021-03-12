fe8bce50e67ef4d8f3aca1b23b3d83ee40a16f96
Marco De Simoni <S4513302@studenti.unige.it>
2021-03-12 20:00:55 +0100

    Aggiungere i file di progetto.

---
+++ TestIteratori/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIteratori
+{
+   public static  class Program
+    {
+
+        public static IEnumerable<T> Prova<T> (T [] a)
+        {
+            if(a==null)
+                throw new ArgumentNullException();
+            return null;
+        }
+        public static IEnumerable<T> MacroExpansion<T>(T[] input_sequence, T value, T[] new_sequence)
+        {
+            if (null == input_sequence)
+                throw new ArgumentNullException("the input sequence is null");
+            if (null == new_sequence)
+                throw new ArgumentNullException("the new sequence is null");
+            if (!input_sequence.Contains(value))
+                throw new ArgumentException("value isn't in the input sequence");
+            int i = 0;
+            while (i < input_sequence.Length  /*!= null*/)
+            {
+                if (input_sequence[i].Equals(value) )
+                {
+                    int j = 0;
+                    while (j < new_sequence.Length  /*!= null*/)
+                    {
+                        yield return new_sequence[j];
+                        ++j;
+                    }
+                }
+                else
+                {
+                    yield return input_sequence[i];
+                  
+                }
+                ++i;
+                    
+            }
+        }
+
+        static void Main(string[] args) { 
+              try
+              {
+                  foreach (var i in MacroExpansion( /*new[] {1, 2, 1, 2, 3}*/null, 2, new[] {7, 8, 9}))
+                  {
+                      Console.WriteLine(i);
+                      Console.ReadLine();
+                  }
+              }
+              catch (ArgumentNullException ex)
+              {
+                  Console.WriteLine(ex.Message);
+                  Console.ReadLine();
+              }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using TestIteratori;
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        
+        public void Sequence_Null_Throws_ArgumentException()
+        {
+            var i = Program.MacroExpansion(null, 2, new[] { 7, 8, 9 });
+            Assert.That( ()=> i.First(),Throws.TypeOf<ArgumentNullException>());
+            // Assert.Throws<ArgumentNullException>(() => Program.MacroExpansion(null, 2, new int[] { 7, 8, 9 }));
+        }
+
+        [Test]
+        public void NewSequence_Null_Throws_ArgumentException()
+        {
+            var i = Program.MacroExpansion(new[] { 1, 2, 1, 2, 3 }, 2, null);
+            Assert.That(() => i.First(), Throws.TypeOf<ArgumentNullException>());
+            // Assert.Throws<ArgumentNullException>(() => Program.MacroExpansion(null, 2, new int[] { 7, 8, 9 }));
+        }
+
+        [Test]
+        public void Value_Not_In_Sequence()
+        {
+            var i = Program.MacroExpansion(new[] {1, 2, 1, 2, 3}, 6, new[] {7, 8, 9});
+            Assert.That(()=>i.First(), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void One_Time()
+        {
+            var seq = new[] {1, 2, 1, 2, 3};
+            var val = 3;
+            var new_seq = new[] {7, 8, 9};
+            var correct_res = new[] {1, 2, 1, 2, 7, 8, 9};
+            var res = new List<int>();
+            foreach (var i in Program.MacroExpansion(seq, val, new_seq))
+            {
+                res.Add(i);
+            }
+
+            Assert.That(res, Is.EqualTo(correct_res));
+
+        }
+
+
+
+
+
+
+
+        [Ignore("prova")]
+        [Test]
+        public void Tre()
+        {
+             Assert.That(() => Program.Prova((int[])null/*new int[]{1}*/), Throws.TypeOf<ArgumentNullException>());
+            //Assert.That(()=>Program.Prova(),Is.EqualTo(3));
+        }
+    }
+}
